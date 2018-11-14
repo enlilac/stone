b419c870eb439b28a9df7443122a05d3a80b4a68
junzhouwu <uwghh@126.com>
2018-11-14 21:01:05 +0800

    test

---
+++ src/test/java/com/enlilac/stone/TestMain.java
@@ -0,0 +1,55 @@
+package com.enlilac.stone;
+
+public class TestMain {
+
+    public static int[] sort(int[] a, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            sort(a, low, mid);
+            sort(a, mid + 1, high);
+            //左右归并
+            merge(a, low, mid, high);
+        }
+        return a;
+    }
+
+    public static void merge(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
+        }
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = a[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        for (int x = 0; x < temp.length; x++) {
+            a[x + low] = temp[x];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0] = 1;
+        a[2] = 10;
+        a[1] = 3;
+        a[3] = 4;
+        a[4] = 2;
+        sort(a, 0, 4);
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
