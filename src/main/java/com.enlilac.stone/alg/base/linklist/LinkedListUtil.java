14b85c6057d56db531c6f6b27b84dc94781a42bd
junzhouwu <uwghh@126.com>
2018-11-14 16:14:37 +0800

    reverse link list

---
+++ src/main/java/com.enlilac.stone/alg/base/linklist/LinkedListUtil.java
@@ -0,0 +1,127 @@
+package com.enlilac.stone.alg.base.linklist;
+
+/**
+ * 单链表链表操作训练
+ */
+public class LinkedListUtil {
+
+
+    /**
+     * 逆转单链表
+     *
+     * @param head
+     * @param <T>
+     * @return
+     */
+    public static <T> Node<T> reverse(Node<T> head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        /**
+         * three point : prev,curr,next
+         * curr is the result head
+         */
+        Node<T> prev = null;
+        Node<T> curr = head;
+        Node<T> next = curr.next;
+        while (curr != null) {
+            Node tmp = curr.next; // save the next node
+            curr.next = prev; // reverse current node
+            if (tmp == null) {
+                break;
+            }
+            prev = curr; // prev to current node
+            curr = tmp; // current node to next
+            next = next.next; // next to next node
+        }
+        return curr;
+    }
+
+
+    public static void testReverse() {
+
+        Node<Integer> head = null;
+        System.out.println(reverse(head));
+        head = new Node<Integer>(1);
+        System.out.println(reverse(head));
+    }
+
+    public static void testReverse2() {
+
+        Node<Integer> head = null;
+        head = new Node<Integer>(1);
+        Node<Integer> next = new Node<Integer>(2);
+        head.next = next;
+        System.out.println(reverse(head));
+    }
+
+    public static void testReverse3() {
+
+        Node<Integer> head = null;
+        head = new Node<Integer>(1);
+        Node<Integer> next = new Node<Integer>(2);
+        Node<Integer> third = new Node<Integer>(3);
+        head.next = next;
+        next.next = third;
+        System.out.println(reverse(head));
+    }
+
+    public static void main(String[] args) {
+        testReverse();
+        testReverse2();
+        testReverse3();
+    }
+
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node() {
+        }
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder("Node [");
+            sb.append(data);
+            sb.append(",");
+            while (next != null) {
+                sb.append(next.data);
+                sb.append(",");
+                next = next.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+}
+
+
+
